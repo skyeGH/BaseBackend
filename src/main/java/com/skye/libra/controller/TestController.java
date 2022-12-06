6b8f6dfdb36682cc04e2fc48cb06cd7388e39d81
gaihao <gaihao@homolo.com>
2022-12-06 10:58:45 +0800

    feat: 增加测试用例

--- src/main/java/com/skye/libra/controller/TestController.java
+++ src/main/java/com/skye/libra/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.skye.libra.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,8 @@ import java.util.Map;
 @RequestMapping("/test")
 public class TestController {
 
+	@GetMapping("get")
+	public Object getTest() {
+		return "Hello World!";
+	}
 }
